f5425a947ac886912f141ac07ac29816b533ec4d
Korchev <Korchev94@gmail.com>
2021-01-09 20:53:48 +0000

    changes

--- app.js
+++ app.js
@@ -4,31 +4,31 @@ const video = document.querySelector(".nasaVideo");
 const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 const div = document.querySelector(".imgDiv");
-const h2 = document.querySelector('.title')
-const theDate = document.querySelector('.date')
-const explanation = document.querySelector('.explanation')
+const h2 = document.querySelector(".title");
+const theDate = document.querySelector(".date");
+const explanation = document.querySelector(".explanation");
 
 const getImage = async () => {
   let response = await fetch(
     `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${input.value}`
   );
-  let res = await response.json()
+  let res = await response.json();
   if (res.media_type == "image") {
-  video.classList.add('displayNone')
-  image.src = res.url
-  image.classList.remove('displayNone')
-  console.log(res)
+    video.classList.add("displayNone");
+    image.src = res.url;
+    image.classList.remove("displayNone");
+    console.log(res);
   } else {
-    image.classList.add('displayNone')
-    video.classList.remove('displayNone')
-    video.src = res.url
+    image.classList.add("displayNone");
+    video.classList.remove("displayNone");
+    video.src = res.url;
   }
-  h2.textContent = res.title
-  explanation.textContent = res.explanation
+  h2.textContent = res.title;
+  explanation.textContent = res.explanation;
 };
 
 btn.addEventListener("click", (e) => {
-  e.preventDefault()
+  e.preventDefault();
   getImage();
-  div.classList.remove('displayNone')
+  div.classList.remove("displayNone");
 });
