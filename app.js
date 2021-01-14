26a5f54b525d1d772c7430b337edfb020903597a
Korchev <Korchev94@gmail.com>
2021-01-14 08:26:07 +0000

    changes light/dark mode added, styles updated

--- app.js
+++ app.js
@@ -2,11 +2,33 @@ const API_KEY = "CW9w2ngA4gOBwh4WpNgtZvvdGrUSP25jzvWL0b3P";
 const image = document.querySelector(".nasaImg");
 const video = document.querySelector(".nasaVideo");
 const input = document.querySelector(".input");
-const btn = document.querySelector(".btn");
+const btn = document.querySelector(".show-btn");
+const darkModeBtn = document.querySelector(".dark-mode");
 const div = document.querySelector(".imgDiv");
 const h2 = document.querySelector(".title");
 const theDate = document.querySelector(".date");
 const explanation = document.querySelector(".explanation");
+const body = document.querySelector('body')
+const main = document.querySelector('#main')
+
+
+
+darkModeBtn.addEventListener('click', e => {
+  e.preventDefault()
+  if(!body.classList.contains('light')) {
+    body.classList.add('light')
+    darkModeBtn.textContent= "Dark Mode"
+    darkModeBtn.classList.add('dark-mode-click')
+  } else {
+    body.classList.remove('light')
+    darkModeBtn.textContent= "Light Mode"
+    darkModeBtn.classList.remove('dark-mode-click')
+
+  }
+  
+
+})
+
 
 const getImage = async () => {
   let response = await fetch(
